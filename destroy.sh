4eaec79542403136edf35b61f2ffe4f565d197e7
Yongkang <he@yongkang.cloud>
2021-09-04 21:52:24 +0800

    Update destroy scripts

--- destroy.sh
+++ destroy.sh
@@ -5,10 +5,10 @@ gkeclustername=$(gcloud container clusters list | grep $MY_CLUSTER | awk '{print
 gcloud container clusters delete $gkeclustername --zone $MY_ZONE --quiet
 
 echo '-------Deleting disks'
-for i in $(gcloud compute disks list --filter="zone:($MY_ZONE)" | grep $gkeclustername | awk '{print $1}');do echo $i;gcloud compute disks delete $i --zone=$MY_ZONE -q;done
+for i in $(gcloud compute disks list --filter="zone:($MY_ZONE)" | grep $MY_CLUSTER | awk '{print $1}');do echo $i;gcloud compute disks delete $i --zone=$MY_ZONE -q;done
 
 echo '-------Deleting snapshots'
-for i in $(gcloud compute snapshots list | grep $gkeclustername | awk '{print $1}');do echo $i;gcloud compute snapshots delete $i -q;done
+for i in $(gcloud compute snapshots list | grep $MY_CLUSTER | awk '{print $1}');do echo $i;gcloud compute snapshots delete $i -q;done
 
 echo '-------Deleting objects from the bucket'
 myproject=$(gcloud config get-value core/project)
