d5ac143d2ea2af9865e4bcc9fda7d3a9e8329251
Yongkang He <he@yongkang.cloud>
2021-09-10 15:48:04 +0800

    Update createsa.sh

--- createsa.sh
+++ createsa.sh
@@ -3,4 +3,5 @@ myproject=$(gcloud config get-value core/project)
 gcloud iam service-accounts create k10-yong-sa --display-name "K10 Service Account"
 k10saemail=$(gcloud iam service-accounts list --filter "k10-yong-sa" --format="value(email)")
 gcloud iam service-accounts keys create --iam-account=${k10saemail} k10-sa-key.json
-gcloud projects add-iam-policy-binding ${myproject} --member serviceAccount:${k10saemail} --role roles/compute.storageAdmin
+gcloud projects add-iam-policy-binding ${myproject} --member serviceAccount:${k10saemail} --role "roles/compute.storageAdmin"
+gcloud projects add-iam-policy-binding ${myproject} --member serviceAccount:${k10saemail} --role "roles/storage.admin"
