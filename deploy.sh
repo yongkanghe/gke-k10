aa1722e2c503986b43d8d26383e0fbfe95976375
Yongkang He <he@yongkang.cloud>
2021-09-11 17:33:52 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -40,7 +40,7 @@ helm install k10 kasten/k10 --namespace=kasten-io \
 echo '-------Set the default ns to k10'
 kubectl config set-context --current --namespace kasten-io
 
-echo '-------Waiting for K10 services are up running'
+echo '-------Waiting for K10 services are up running in about 2 mins'
 kubectl wait --for=condition=ready --timeout=180s -n kasten-io pod -l component=catalog
 
 echo '-------Output the Cluster ID, Web UI IP and token'
