292b6201125155361f7df50179fad7450d036df4
Yongkang He <he@yongkang.cloud>
2021-09-11 14:33:01 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,8 @@
 #-------Set the environment variables"
-export MY_CLUSTER=gke4yong1 #Change to your favorite cluster name
-export MY_MACHINE_TYPE=e2-standard-2 #Change to your favorite machine type
-export MY_REGION=us-central1 #Change to your favorite region
-export MY_ZONE=us-central1-f #Change to your favorite zone
-export MY_BUCKET=k10bucket4yong1 #Change to your favorite bucket, unique name in your project
-export MY_OBJECT_STORAGE_PROFILE=mygcs1 #Change to your favorite profile name
-export K8S_VERSION=1.20 #Change to your favorite Kubernetes version
+export MY_CLUSTER=gke4yong1 #Customize your favorite cluster name
+export MY_MACHINE_TYPE=e2-standard-2 #Customize your favorite machine type
+export MY_REGION=us-central1 #Customize your favorite region
+export MY_ZONE=us-central1-f #Customize your favorite zone
+export MY_BUCKET=k10bucket4yong1 #Customize your favorite bucket
+export MY_OBJECT_STORAGE_PROFILE=mygcs1 #Customize your favorite profile name
+export K8S_VERSION=1.20 #Customize your favorite Kubernetes version
