0590e74e6b8ee49671d874049f9f7c5c429bdf4c
Yongkang He <he@yongkang.cloud>
2021-09-11 15:48:45 +0800

    Updated deploy.sh & setenv.sh

--- deploy.sh
+++ deploy.sh
@@ -2,13 +2,13 @@ echo '-------Creating a GKE Cluster (typically in less than 10 mins)'
 starttime=$(date +%s)
 . setenv.sh
 MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
-GCP_K8S_VERSION=$(gcloud container get-server-config --flatten="channels" --filter="channels.channel=REGULAR" --region $MY_REGION | grep $MY_K8S_VERSION | awk '{print $2}' | sort | uniq | tail -1)
+#GCP_K8S_VERSION=$(gcloud container get-server-config --flatten="channels" --filter="channels.channel=REGULAR" --region $MY_REGION | grep $MY_K8S_VERSION | awk '{print $2}' | sort | uniq | tail -1)
 gcloud container clusters create $MY_PREFIX-$MY_CLUSTER-$(date +%s) \
   --zone $MY_ZONE \
   --num-nodes 1 \
   --machine-type $MY_MACHINE_TYPE \
   --release-channel=regular \
-  --cluster-version=$GCP_K8S_VERSION \
+  --cluster-version=$MY_K8S_VERSION-gke.701 \
   --no-enable-basic-auth \
   --addons=GcePersistentDiskCsiDriver \
   --enable-autoscaling --min-nodes 1 --max-nodes 3
