b1929d1e9a714079b149c703b9350ecaf645102c
Yongkang He <he@yongkang.cloud>
2021-09-11 14:29:16 +0800

    Update setenv.sh

--- setenv.sh
+++ setenv.sh
@@ -5,3 +5,4 @@ export MY_REGION=us-central1 #Change to your favorite region
 export MY_ZONE=us-central1-f #Change to your favorite zone
 export MY_BUCKET=k10bucket4yong1 #Change to your favorite bucket, unique name in your project
 export MY_OBJECT_STORAGE_PROFILE=mygcs1 #Change to your favorite profile name
+export K8S_VERSION=1.20 #Change to your favorite Kubernetes version
