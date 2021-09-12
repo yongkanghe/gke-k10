60338c68553d9f209ed0030d4f15a28c04d7438f
Yongkang He <he@yongkang.cloud>
2021-09-12 13:13:28 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -51,7 +51,7 @@ k10ui=http://$(kubectl get svc gateway-ext | awk '{print $4}'|grep -v EXTERNAL)/
 echo -e "\nCopy below token code before Click the link to log into K10 Web UI -->> $k10ui" >> gke-token
 echo "" | awk '{print $1}' >> gke-token
 sa_secret=$(kubectl get serviceaccount k10-k10 -o jsonpath="{.secrets[0].name}" --namespace kasten-io)
-echo "Please enter below token to login" >> gke-token
+echo "Here is the token to login K10 Web UI" >> gke-token
 echo "" | awk '{print $1}' >> gke-token
 kubectl get secret $sa_secret --namespace kasten-io -ojsonpath="{.data.token}{'\n'}" | base64 --decode | awk '{print $1}' >> gke-token
 echo "" | awk '{print $1}' >> gke-token
