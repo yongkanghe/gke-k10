ec2b2be6f244ad0a197624b20ab7608f3454b5c8
Yongkang He <he@yongkang.cloud>
2021-09-12 15:21:09 +0800

    Update createsa.sh

--- createsa.sh
+++ createsa.sh
@@ -1,10 +1,12 @@
-#-------Creating a Google Cloud Service Account"
+echo "-------Creating a Google Cloud Service Account"
 myproject=$(gcloud config get-value core/project)
 gcloud iam service-accounts create k10-yong-sa --display-name "K10 Service Account"
 k10saemail=$(gcloud iam service-accounts list --filter "k10-yong-sa" --format="value(email)")
 gcloud iam service-accounts keys create --iam-account=${k10saemail} k10-sa-key.json
 gcloud projects add-iam-policy-binding ${myproject} --member serviceAccount:${k10saemail} --role "roles/compute.storageAdmin"
 gcloud projects add-iam-policy-binding ${myproject} --member serviceAccount:${k10saemail} --role "roles/storage.admin"
-#-------Enabling Google Kubernetes Engine API"
+echo "-------Enabling Google Kubernetes Engine API"
 gcloud services enable container.googleapis.com
+clear
+echo "-------Google Cloud service account is ready and GKE API enabled"
 
