288230b7b26fd4643c7e48b4d3f192b08e161ecc
Yongkang He <he@yongkang.cloud>
2021-12-01 14:35:29 +0800

    Update k10-deploy.sh

--- k10-deploy.sh
+++ k10-deploy.sh
@@ -24,7 +24,8 @@ helm install k10 kasten/k10 --namespace=kasten-io \
   --set global.persistence.grafana.size=1Gi \ 
   --set secrets.googleApiKey=$sa_key \
   --set auth.tokenAuth.enabled=true \
-  --set externalGateway.create=true
+  --set externalGateway.create=true \
+  --set metering.mode=airgap
 
 echo '-------Set the default ns to k10'
 kubectl config set-context --current --namespace kasten-io
