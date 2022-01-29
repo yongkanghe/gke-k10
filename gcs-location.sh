a108fdccfaed0fa862bfea163b78cf6a6ffe9e9d
Yongkang He <he@yongkang.cloud>
2022-01-29 14:51:28 +0800

    Create gcs-location.sh

---
+++ gcs-location.sh
@@ -0,0 +1,33 @@
+. ./setenv.sh
+MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+
+echo '-------Creating a GCS profile secret'
+myproject=$(gcloud config get-value core/project)
+kubectl create secret generic k10-gcs-secret \
+      --namespace kasten-io \
+      --from-literal=project-id=$myproject \
+      --from-file=service-account.json=k10-sa-key.json
+
+echo '-------Creating a GCS profile'
+cat <<EOF | kubectl apply -f -
+apiVersion: config.kio.kasten.io/v1alpha1
+kind: Profile
+metadata:
+  name: $MY_OBJECT_STORAGE_PROFILE
+  namespace: kasten-io
+spec:
+  type: Location
+  locationSpec:
+    credential:
+      secretType: GcpServiceAccountKey
+      secret:
+        apiVersion: v1
+        kind: Secret
+        name: k10-gcs-secret
+        namespace: kasten-io
+    type: ObjectStore
+    objectStore:
+      name: $MY_PREFIX-$MY_BUCKET
+      objectStoreType: GCS
+      region: $MY_REGION
+EOF
