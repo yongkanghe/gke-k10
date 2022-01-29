f004b86e367ed540d641dbcd5f01dc05221cdd87
Yongkang He <he@yongkang.cloud>
2022-01-29 15:12:10 +0800

    Update k10-destroy.sh

--- k10-destroy.sh
+++ k10-destroy.sh
@@ -1,6 +1,6 @@
 starttime=$(date +%s)
 . ./setenv.sh
-echo '-------Deleting the GKE Cluster (typically in less than 10 mins)'
+echo '-------Deleting the GKE Cluster (typically in few mins)'
 MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
 gkeclustername=$(gcloud container clusters list --format="value(name)" --filter="$MY_PREFIX-$MY_CLUSTER")
 #gcloud container clusters delete $gkeclustername --zone $MY_ZONE --quiet
