f952986c313082013a57658eaa3d7616d954e2b8
Yongkang He <he@yongkang.cloud>
2022-01-29 14:51:34 +0800

    Create postgresql-policy.sh

---
+++ postgresql-policy.sh
@@ -0,0 +1,66 @@
+. ./setenv.sh
+MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+
+echo '------Create backup policies'
+cat <<EOF | kubectl apply -f -
+apiVersion: config.kio.kasten.io/v1alpha1
+kind: Policy
+metadata:
+  name: k10-postgresql-backup
+  namespace: kasten-io
+spec:
+  comment: ""
+  frequency: "@hourly"
+  actions:
+    - action: backup
+      backupParameters:
+        profile:
+          namespace: kasten-io
+          name: mygcs1
+    - action: export
+      exportParameters:
+        frequency: "@hourly"
+        migrationToken:
+          name: ""
+          namespace: ""
+        profile:
+          name: mygcs1
+          namespace: kasten-io
+        receiveString: ""
+        exportData:
+          enabled: true
+      retention:
+        hourly: 0
+        daily: 0
+        weekly: 0
+        monthly: 0
+        yearly: 0
+  retention:
+    hourly: 4
+    daily: 1
+    weekly: 1
+    monthly: 0
+    yearly: 0
+  selector:
+    matchExpressions:
+      - key: k10.kasten.io/appNamespace
+        operator: In
+        values:
+          - k10-postgresql
+EOF
+
+sleep 5
+
+echo '-------Kickoff the on-demand backup job'
+sleep 5
+cat <<EOF | kubectl create -f -
+apiVersion: actions.kio.kasten.io/v1alpha1
+kind: RunAction
+metadata:
+  generateName: run-backup-
+spec:
+  subject:
+    kind: Policy
+    name: k10-postgresql-backup
+    namespace: kasten-io
+EOF
