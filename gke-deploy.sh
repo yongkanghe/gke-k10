e7a0f8469ea0277e148a368e613ed8391c29beb8
Yongkang He <he@yongkang.cloud>
2022-04-24 15:29:36 +0800

    Update gke-deploy.sh

--- gke-deploy.sh
+++ gke-deploy.sh
@@ -1,7 +1,10 @@
 echo '-------Creating a GKE Cluster only (typically in 4 mins)'
 starttime=$(date +%s)
 . ./setenv.sh
-MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+TEMP_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+FIRST2=$(echo -n $TEMP_PREFIX | head -c2)
+LAST2=$(echo -n $TEMP_PREFIX | tail -c2)
+MY_PREFIX=$(echo $FIRST2$LAST2)
 
 gcloud container clusters create $MY_PREFIX-$MY_CLUSTER-$(date +%s) \
   --zone $MY_ZONE \
