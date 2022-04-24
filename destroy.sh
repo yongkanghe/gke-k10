66a5ae11d2579a80ee2bc33a89a38b66c96eec43
Yongkang He <he@yongkang.cloud>
2022-04-24 15:29:26 +0800

    Update destroy.sh

--- destroy.sh
+++ destroy.sh
@@ -1,15 +1,20 @@
 starttime=$(date +%s)
 . ./setenv.sh
 echo '-------Deleting the GKE Cluster (typically in few mins)'
-MY_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+TEMP_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print tolower($0)}')
+FIRST2=$(echo -n $TEMP_PREFIX | head -c2)
+LAST2=$(echo -n $TEMP_PREFIX | tail -c2)
+MY_PREFIX=$(echo $FIRST2$LAST2)
+
 gkeclustername=$(gcloud container clusters list --format="value(name)" --filter="$MY_PREFIX-$MY_CLUSTER")
 gcloud container clusters delete $gkeclustername --zone $MY_ZONE --quiet
+findmydisk=$(echo $MY_PREFIX-$MY_CLUSTER | head -c 12)
 
 echo '-------Deleting disks'
-for i in $(gcloud compute disks list --format="value(name)" --filter="$MY_PREFIX-$MY_CLUSTER");do echo $i;gcloud compute disks delete $i --zone=$MY_ZONE -q;done
+for i in $(gcloud compute disks list --format="value(name)" --filter="$findmydisk");do echo $i;gcloud compute disks delete $i --zone=$MY_ZONE -q;done
 
 echo '-------Deleting snapshots'
-for i in $(gcloud compute snapshots list --format="value(name)" --filter="$MY_PREFIX-$MY_CLUSTER");do echo $i;gcloud compute snapshots delete $i -q;done
+for i in $(gcloud compute snapshots list --format="value(name)" --filter="$findmydisk");do echo $i;gcloud compute snapshots delete $i -q;done
 
 echo '-------Deleting the bucket'
 myproject=$(gcloud config get-value core/project)
