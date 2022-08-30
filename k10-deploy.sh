a03c1c27967d501836bdb1d9f19fcfb2040f8c98
Yongkang He <he@yongkang.cloud>
2022-08-30 12:26:01 +0800

    Update k10-deploy.sh

--- k10-deploy.sh
+++ k10-deploy.sh
@@ -12,9 +12,13 @@ kubectl create ns kasten-io
 helm repo add kasten https://charts.kasten.io/
 helm repo update
 
+echo '-------Set the default sc & vsc'
+kubectl annotate sc standard storageclass.kubernetes.io/is-default-class-
+kubectl annotate sc standard-rwo storageclass.kubernetes.io/is-default-class=true --overwrite
+
 #For Production, remove the lines ending with =1Gi from helm install
 #For Production, remove the lines ending with airgap from helm install
-helm install k10 kasten/k10 --namespace=kasten-io \
+helm install k10 kasten/k10 --version=5.0.6 --namespace=kasten-io \
   --set global.persistence.metering.size=1Gi \
   --set prometheus.server.persistentVolume.size=1Gi \
   --set global.persistence.catalog.size=1Gi \
