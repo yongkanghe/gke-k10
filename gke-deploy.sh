f5e51f0a40d2343aec596203d0cfc03361b6a896
Yongkang He <he@yongkang.cloud>
2022-11-04 11:01:30 +0800

    Update gke-deploy.sh

--- gke-deploy.sh
+++ gke-deploy.sh
@@ -5,13 +5,14 @@ TEMP_PREFIX=$(echo $(whoami) | sed -e 's/\_//g' | sed -e 's/\.//g' | awk '{print
 FIRST2=$(echo -n $TEMP_PREFIX | head -c2)
 LAST2=$(echo -n $TEMP_PREFIX | tail -c2)
 MY_PREFIX=$(echo $FIRST2$LAST2)
+GKE_VERSION=$(gcloud container get-server-config --region us-central1 --flatten="channels" --filter="channels.channel=RAPID" | grep 1.25 | head -1 | awk '{print $2}')
 
 gcloud container clusters create $MY_PREFIX-$MY_CLUSTER-$(date +%s) \
   --zone $MY_ZONE \
   --num-nodes 1 \
   --machine-type $MY_MACHINE_TYPE \
   --release-channel=rapid \
-  --cluster-version 1.25.0-gke.1100 \
+  --cluster-version $GKE_VERSION \
   --no-enable-basic-auth \
   --addons=GcePersistentDiskCsiDriver \
   --enable-autoscaling --min-nodes 1 --max-nodes 3
